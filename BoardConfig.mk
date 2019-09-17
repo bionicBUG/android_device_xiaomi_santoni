bd30c38b25fdc6f59bf7fe689ead0c4c85a2fcfc
Ojas Sinha <sinhaojas67@gmail.com>
2019-09-17 15:35:06 +0000

    santoni: more permissive and update init.early_boot
    i forgot to do these changes earlier XD
    Signed-off-by: Ojas Sinha <sinhaojas67@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -225,8 +225,8 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/recovery/fstab.qcom
 
 # SELinux
--include device/qcom/sepolicy/sepolicy.mk
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+#include device/qcom/sepolicy/sepolicy.mk
+#BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 #BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 #BOARD_SEPOLICY_VERS := 28.0
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/minimal
