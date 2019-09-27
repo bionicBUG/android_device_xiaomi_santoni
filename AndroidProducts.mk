08930636a0f30a15383636f6c75d8eb6dc707743
Ojas Sinha <sinhaojas67@gmail.com>
2019-09-27 08:24:38 +0000

    henlo pixys

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/full_santoni.mk \
-    $(LOCAL_DIR)/aosp_santoni.mk
+    $(LOCAL_DIR)/pixys_santoni.mk
 
-COMMON_LUNCH_CHOICES := aosp_santoni-userdebug
+COMMON_LUNCH_CHOICES := pixys_santoni-userdebug
--- pixys_santoni.mk
+++ pixys_santoni.mk
@@ -16,16 +16,16 @@
 
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
-
+TARGET_BOOT_ANIMATION_RES := 720
 $(call inherit-product, device/xiaomi/santoni/full_santoni.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
 
 PRODUCT_DEVICE := santoni
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 4X
-PRODUCT_NAME := lineage_santoni
+PRODUCT_NAME := pixys_santoni
 BOARD_VENDOR := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
 
